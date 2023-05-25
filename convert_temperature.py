1bb188da04d14634c07254381e91537b22d2435e
xkouoi <x.kouoi@tudelft.nl>
2023-05-25 14:22:26 +0200

    addition of a description block

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,3 +1,10 @@
 def fahrenheit_to_celsius(temperature_fahr):
+	"""
+	Converts and input argument of a temperature in degrees Fahrenheit and returns a temperature value in degrees Celsius
+	Parameters: temperature un Fahr
+
+	Returns: temperature in Celsius
+	"""
+
 	temperature_celsius = (temperature_fahr-32)*(5.0/9.0)
 	return temp_celsius
